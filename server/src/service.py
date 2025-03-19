e3b4d35b9598a724d8af99449a0a953ff9ef153e
Azizi <64193693+azizi-a@users.noreply.github.com>
2025-03-19 11:14:18 +0000

    update user content rating on vote
    - Added `update_user_embedding` function to modify user embeddings in the database.
    - Introduced `update_user_content_rating` function to handle both updating and inserting user ratings for content.
    - Refactored `handle_feedback` to utilize the new functions for better modularity and clarity.
    - Improved code readability by restructuring embedding update logic.

--- server/src/service.py
+++ server/src/service.py
@@ -93,7 +93,7 @@ async def get_recommendations(user_id: int):
 #
 #
 #
-async def update_user_embedding(user_id: int, updated_embedding: list):
+async def update_user_embedding(user_id: int, updated_embedding: torch.Tensor):
   db = await database.get_db()
   await db.execute(database.users.update().where(database.users.c.id == user_id), {"embedding": updated_embedding})
 
@@ -142,10 +142,11 @@ async def handle_feedback(user_id: int, content_id: int, rating: float):
   content = await db.fetch_one(database.content.select().where(database.content.c.id == content_id))
 
   # Adjust the embedding based on the rating using exponential moving average (EMA)
-  updated_embedding = constants.USER_ADJUST_FACTOR * content.embedding * rating + \
-                      ((1 - constants.USER_ADJUST_FACTOR) * user.embedding)
+  updated_embedding = constants.USER_ADJUST_FACTOR * content.embedding * rating + (
+    (1 - constants.USER_ADJUST_FACTOR) * user.embedding
+  )
 
-  await update_user_embedding(user_id, updated_embedding.tolist())
+  await update_user_embedding(user_id, updated_embedding)
   await update_user_content_rating(user_id, content_id, rating)
 
 
