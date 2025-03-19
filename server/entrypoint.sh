558510002efa504338836a4b05fec70e0d9280c4
Cameron <48329714+CNimmo16@users.noreply.github.com>
2025-03-19 14:31:57 +0000

    better exit code check

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,9 +4,10 @@
 printenv | grep -E 'DATABASE_URL|CLERK_SECRET_KEY' | sed 's/^/export /' > /etc/cron.env
 
 # Run migrations
-exit_code=$(python -m alembic upgrade head)
+python -m alembic upgrade head
+exit_code=$?
 
-if [ $exit_code -ne 0 ]; then
+if [ "$exit_code" -ne 0 ]; then
   echo "Failed to run migrations"
   exit $exit_code
 fi
