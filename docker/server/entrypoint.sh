007e080241153b0f2b6c69f461ae379dd8a9930b
Cameron <48329714+CNimmo16@users.noreply.github.com>
2025-03-25 16:56:26 +0000

    move server entrypoint files out of image

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH="/app/.venv/bin:$PATH"
+
 # Export all current environment variables to a file so cron jobs can use them
 printenv | grep -E 'DATABASE_URL|CLERK_SECRET_KEY|OLLAMA_HOST' | sed 's/^/export /' > /etc/cron.env
 
