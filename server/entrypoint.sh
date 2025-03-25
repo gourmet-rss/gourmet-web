4c664751ffc57337a06b95725d09858a3544e641
Cameron <48329714+CNimmo16@users.noreply.github.com>
2025-03-25 16:29:07 +0000

    pass OLLAMA_HOST env to cron

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export all current environment variables to a file so cron jobs can use them
-printenv | grep -E 'DATABASE_URL|CLERK_SECRET_KEY' | sed 's/^/export /' > /etc/cron.env
+printenv | grep -E 'DATABASE_URL|CLERK_SECRET_KEY|OLLAMA_HOST' | sed 's/^/export /' > /etc/cron.env
 
 echo "Running migrations..."
 echo "DATABASE_URL: ${DATABASE_URL}"
